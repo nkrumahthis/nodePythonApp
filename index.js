588d19d0ae9834af00ceb2077b49af22a6bcbbe5
nkrumahthis <nkrumahthis@gmail.com>
2020-12-11 13:48:28 +0000

    chore: clean up, only index.js only does script3 now

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    // var dataToSend;
     var largeDataSet=[];
     
     // spawn new child process to call the python script
-    // const python = spawn('python', ['script1.py']);
-    // const python = spawn('python', ['script2.py', 'node.js', 'python']);
     const python = spawn('python', ['script3.py']);
     
     // collect data from script
     python.stdout.on('data', function(data){
         console.log('Pipe data from python script...');
-        // dataToSend = data.toString();
         largeDataSet.push(data);
     });
     
@@ -23,7 +19,6 @@ app.get('/', (req, res) => {
     python.on('close', (code)=>{
         console.log(`child process close all stdio with code ${code}`)
          // send data to browser
-        //  res.send(dataToSend)
         res.send(largeDataSet.join(""));
     });
 
