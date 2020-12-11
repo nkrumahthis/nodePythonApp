638af173a79dd7b37cbd189f5e9278553e59db5b
nkrumahthis <nkrumahthis@gmail.com>
2020-12-11 13:46:43 +0000

    dev: get json dataset from python

--- index.js
+++ index.js
@@ -4,23 +4,27 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    var dataToSend;
+    // var dataToSend;
+    var largeDataSet=[];
     
     // spawn new child process to call the python script
     // const python = spawn('python', ['script1.py']);
-    const python = spawn('python', ['script2.py', 'node.js', 'python']);
+    // const python = spawn('python', ['script2.py', 'node.js', 'python']);
+    const python = spawn('python', ['script3.py']);
     
     // collect data from script
     python.stdout.on('data', function(data){
         console.log('Pipe data from python script...');
-        dataToSend = data.toString();
+        // dataToSend = data.toString();
+        largeDataSet.push(data);
     });
     
     // in close event we are sure that stream from child process is closed
     python.on('close', (code)=>{
         console.log(`child process close all stdio with code ${code}`)
          // send data to browser
-         res.send(dataToSend)
+        //  res.send(dataToSend)
+        res.send(largeDataSet.join(""));
     });
 
 });
